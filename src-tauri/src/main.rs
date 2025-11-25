7de8d6a5cbbbd9193b9cf8ba7a1b5efc562113e3
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-11-26 01:41:20 +0200

    watcher missing dir fallback and new api integration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod watcher;
 mod im_command;
+mod watcher;
 
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::sync::Mutex;
-use notify::{RecursiveMode, RecommendedWatcher, Watcher};
 use tauri::Manager;
 
 #[derive(Default)]
@@ -13,34 +13,32 @@ struct WatcherKeeper(Mutex<Option<RecommendedWatcher>>);
 
 #[tauri::command] // Ship a custom command to the FE
 fn read_saved_variables(path: String) -> Result<String, String> {
-    std::fs::read_to_string(&path)
-        .map_err(|e| format!("Failed reading file: {e}"))
+    std::fs::read_to_string(&path).map_err(|e| format!("Failed reading file: {e}"))
 }
-
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .manage(WatcherKeeper::default())
         .setup(|app| {
             let handle = app.handle().clone();
 
             let root = std::path::PathBuf::from(
-                r"Z:\Battle.NET Lyb\World of Warcraft\_retail_\WTF\Account"
+                r"Z:\Battle.NET Lyb\World of Warcraft\_retail_\WTF\Account",
             );
 
             if root.exists() {
+                let mut watcher =
+                    watcher::create_watcher(handle).expect("failed to create watcher");
 
-                let mut watcher = watcher::create_watcher(handle)
-                    .expect("failed to create watcher");
-    
                 watcher
                     .watch(&root, RecursiveMode::Recursive)
                     .expect("failed to watch folder");
-    
+
                 // wathcer in mem stored
                 let keeper = app.state::<WatcherKeeper>();
                 *keeper.0.lock().unwrap() = Some(watcher);
-    
+
                 println!("Watching {:?}", root);
             } else {
                 println!("The root path does not exist. The watcher won't register.")
@@ -48,7 +46,10 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![read_saved_variables, im_command::identify_match])
+        .invoke_handler(tauri::generate_handler![
+            read_saved_variables,
+            im_command::identify_match
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri app");
 }
