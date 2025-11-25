24d5ab0923d87db923620e3f9fd87e9ddb83834d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-11-25 10:47:16 +0200

    types that suit our addon they are defined in the build time where the addon build the data this is just TS Interfaces & types to suit our addon data output

---
+++ src/Interfaces/matches.ts
@@ -0,0 +1,46 @@
+export interface Player {
+    name: string;
+    realm: string; // slugified realm
+    class: string; // WARRIOR, MAGE, ROGUE...
+    spec: string; // Protection, Arms, Frost...
+    faction: number; // 0 Alliance, 1 Horde
+    rating: number | null;
+    ratingChange: number | null;
+    prematchMMR: number | null;
+    postmatchMMR: number | null;
+    damage: number;
+    healing: number;
+    kills: number;
+    deaths: number;
+    isOwner: boolean;
+    pvpTalents?: number[]; // only present for owner
+}
+
+export type Interrupts = Record<
+    string, // srcName
+    Record<
+        string, // dstName
+        Record<string, number> // interruptedSpell: count
+    >
+>;
+
+export type Auras = Record<
+    string, // srcName
+    Record<
+        string, // dstName
+        Record<string, number> // auraName: count
+    >
+>;
+
+export interface MatchDetails {
+    timestamp: string; // "2025-02-14 21:17:05"
+    format: string; // "Solo Shuffle", "RBG", "Arena 2v2", etc.
+    mapName: string; // "Nagrand Arena", "Warsong Gulch", etc.
+}
+
+export interface Match {
+    matchDetails: MatchDetails;
+    players: Player[];
+    interrupts?: Interrupts;
+    auras?: Auras;
+}
