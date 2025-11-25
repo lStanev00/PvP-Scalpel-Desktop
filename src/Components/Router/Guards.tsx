8f764770818cdd204fa7d121ee37de85e2751099
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-11-26 01:36:23 +0200

    fixing the guards to suit the app sicne this is another import from https://www.pvpscalpel.com

--- src/Components/Router/Guards.tsx
+++ src/Components/Router/Guards.tsx
@@ -1,12 +1,13 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useUserContext } from "../../Hooks/useUserContext";
+import { Navigate, Outlet } from "react-router-dom";
+import useUserContext from "../../Hooks/useUserContext.ts";
 
 export const UserRoute = () => {
     const { user } = useUserContext();
     const isAuthenticated = Boolean(user?._id);
-    const location = useLocation().pathname;
+    // const location = useLocation().pathname;
 
-    return isAuthenticated ? <Outlet /> : <Navigate to={`/login?target=${location}`} />;
+    // return isAuthenticated ? <Outlet /> : <Navigate to={`/login?target=${location}`} />;
+    return isAuthenticated ? <Outlet /> : <Navigate to={`/login`} />;
 };
 
 export const GuestRoute = () => {
