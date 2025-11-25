64623b8380e68ccd00ab5067108d22dae9647bf9
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-11-26 01:34:20 +0200

    clear tauri v1 deprecated code

--- public/tauri-setups/tray.js
+++ public/tauri-setups/tray.js
@@ -1,20 +1,20 @@
-import { TrayIcon } from '@tauri-apps/api/tray';
-import { Menu } from '@tauri-apps/api/menu';
-import { defaultWindowIcon } from '@tauri-apps/api/app';
+// import { TrayIcon } from '@tauri-apps/api/tray';
+// import { Menu } from '@tauri-apps/api/menu';
+// import { defaultWindowIcon } from '@tauri-apps/api/app';
 
-const menu = await Menu.new({
-  items: [
-    {
-      id: 'quit',
-      text: 'Quit',
-    },
-  ],
-});
+// const menu = await Menu.new({
+//   items: [
+//     {
+//       id: 'quit',
+//       text: 'Quit',
+//     },
+//   ],
+// });
 
-const options = {
-  menu,
-  menuOnLeftClick: true,
-  icon: await defaultWindowIcon()
-};
+// const options = {
+//   menu,
+//   menuOnLeftClick: true,
+//   icon: await defaultWindowIcon()
+// };
 
-const tray = await TrayIcon.new(options);
+// const tray = await TrayIcon.new(options);
